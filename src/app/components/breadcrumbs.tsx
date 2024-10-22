20e508c139d2b52d754a8a960fbfbee0838685d5
TumiKeleps <keleper@telkom.co.za>
2024-10-22 11:40:29 +0200

    breadcrumbs

--- src/app/components/breadcrumbs.tsx
+++ src/app/components/breadcrumbs.tsx
@@ -4,7 +4,7 @@
 import React, { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
-import { Breadcrumbs as MUIBreadcrumbs, Typography, Chip } from '@mui/material';
+import { Breadcrumbs as MUIBreadcrumbs, Typography } from '@mui/material';
 
 const Breadcrumbs: React.FC = () => {
   const pathname = usePathname();
